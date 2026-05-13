f16539bf959801ff5c10dd5f8097f69a0fb0032f
Joshua <joshuawolanyk@gmail.com>
2026-05-13 08:20:23 -0700

    fixing migrate down to drop

--- db/migrations/20260512204852_add_match_eda_col_opp_score_event_type.sql
+++ db/migrations/20260512204852_add_match_eda_col_opp_score_event_type.sql
@@ -4,4 +4,4 @@ ALTER TABLE features.matches_eda ADD COLUMN event_type VARCHAR;
 
 -- migrate:down
 ALTER TABLE features.matches_eda DROP COLUMN opponent_total_score;
-ALTER TABLE features.matches_eda ADD COLUMN event_type;
+ALTER TABLE features.matches_eda DROP COLUMN event_type;
