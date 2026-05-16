27d5fe95be7b19619a6038a58b3b83391c586491
Joshua <joshuawolanyk@gmail.com>
2026-05-15 17:12:39 -0700

    different tests for data science

--- data_science/feature_eng/function_for_feature_eng.py
+++ data_science/feature_eng/function_for_feature_eng.py
@@ -5,7 +5,7 @@ LOOKBACK_RANGE = [3,5,10, 'season']
 MATCH_FUNCTIONS = ['mean', 'max', 'min', 'std', 'trend']
 
 def get_math_list() -> list:
-    col_math_list = ['team_key','match_key', 'last_auto_points']
+    col_math_list = ['team_key','match_key', 'actual_time','last_auto_points']
     for i in LOOKBACK_RANGE:
         for each in MATCH_FUNCTIONS:
             col_math_list.append(f"{each}_{i}_last_auto_points")
@@ -13,11 +13,12 @@ def get_math_list() -> list:
     return col_math_list
 
 
-def get_math_calced_list(team:str, match:str, scores:list[int]) -> list[int]:
+def get_math_calced_list(scores:list[int]) -> list[int]:
     count = len(scores)-1
-    result_list = [team,match]
+    result_list = []
     if count == 0:
-        return [None for i in range(len(get_math_list()))]
+        result_list.append([0 for i in range(len(get_math_list())-3)])
+        return result_list
     result_list.append(scores[-2])
     for i_track in LOOKBACK_RANGE:
         i=i_track
